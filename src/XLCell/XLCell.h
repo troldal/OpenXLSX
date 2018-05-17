9fa39f75da082d541cfb579312407bb875c2d60c
Kenneth Troldal Balslev <kenneth.balslev@gmail.com>
2018-05-17 21:07:14 +0200

    Remake of XLCellValue
    Signed-off-by: Kenneth Troldal Balslev <kenneth.balslev@gmail.com>

--- src/XLCell/XLCell.h
+++ src/XLCell/XLCell.h
@@ -48,7 +48,7 @@ YM      M9  MM    MM MM       MM    MM   d'  `MM.    MM            MM   d'  `MM.
 
 #include <string>
 #include <ostream>
-#include <variant>
+//#include <variant>
 #include "XLCellReference.h"
 #include "../XLWorkbook/XLDocument.h"
 #include "XLCellType.h"
