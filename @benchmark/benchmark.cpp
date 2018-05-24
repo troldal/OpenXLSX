cb615cace082752c4b13d424277a15b5047b3327
troldal <kenneth.balslev@gmail.com>
2018-05-24 16:35:57 +0200

    Included table_printer for benchmarks
    Signed-off-by: troldal <kenneth.balslev@gmail.com>

--- @benchmark/benchmark.cpp
+++ @benchmark/benchmark.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <chrono>
 #include <iomanip>
+#include <sstream>
 #include "table_printer.h"
 #include "../@source/OpenXLSX.h"
 
@@ -9,20 +10,59 @@ using namespace OpenXLSX;
 using bprinter::TablePrinter;
 
 template<typename T>
-unsigned long WriteTest(T value, unsigned long rows, unsigned long columns, int repetitions);
+unsigned long WriteTest(T value, unsigned long rows, unsigned long columns, int repetitions, const std::string &fileName);
 
 int main()
 {
-    WriteTest("Hello, OpenXLSX!", 1000000, 1, 10);
-    WriteTest("Hello, OpenXLSX!", 100000, 10, 10);
-    WriteTest("Hello, OpenXLSX!", 10000, 100, 10);
-    WriteTest("Hello, OpenXLSX!", 1000, 1000, 10);
-    WriteTest("Hello, OpenXLSX!", 100, 10000, 10);
+    cout <<
+    "          ____                               ____      ___ ____       ____  ____      ___\n"
+    "         6MMMMb                              `MM(      )M' `MM'      6MMMMb\\`MM(      )M'\n"
+    "        8P    Y8                              `MM.     d'   MM      6M'    ` `MM.     d'\n"
+    "       6M      Mb __ ____     ____  ___  __    `MM.   d'    MM      MM        `MM.   d'\n"
+    "       MM      MM `M6MMMMb   6MMMMb `MM 6MMb    `MM. d'     MM      YM.        `MM. d'\n"
+    "       MM      MM  MM'  `Mb 6M'  `Mb MMM9 `Mb    `MMd       MM       YMMMMb     `MMd\n"
+    "       MM      MM  MM    MM MM    MM MM'   MM     dMM.      MM           `Mb     dMM.\n"
+    "       MM      MM  MM    MM MMMMMMMM MM    MM    d'`MM.     MM            MM    d'`MM.\n"
+    "       YM      M9  MM    MM MM       MM    MM   d'  `MM.    MM            MM   d'  `MM.\n"
+    "        8b    d8   MM.  ,M9 YM    d9 MM    MM  d'    `MM.   MM    / L    ,M9  d'    `MM.\n"
+    "         YMMMM9    MMYMMM9   YMMMM9 _MM_  _MM_M(_    _)MM_ _MMMMMMM MYMMMM9 _M(_    _)MM_\n"
+    "                   MM\n"
+    "                   MM\n"
+    "                  _MM_" << endl << endl;
+
+    cout << "**************************************";
+    cout << " RUNNING BENCHMARKS ";
+    cout << "**************************************"<< endl << endl;
+
+    //WriteTest("Hello, OpenXLSX!", 1000000, 1, 5, "BenchmarkString.xlsx");
+    //WriteTest("Hello, OpenXLSX!", 100000, 10, 5, "BenchmarkString.xlsx");
+    //WriteTest("Hello, OpenXLSX!", 10000, 100, 5, "BenchmarkString.xlsx");
+    WriteTest("Hello, OpenXLSX!", 1000, 1000, 5, "BenchmarkString.xlsx");
+    //WriteTest("Hello, OpenXLSX!", 100, 10000, 5, "BenchmarkString.xlsx");
+
+    //WriteTest(-42, 1000000, 1, 5, "BenchmarkInteger.xlsx");
+    //WriteTest(-42, 100000, 10, 5, "BenchmarkInteger.xlsx");
+    //WriteTest(-42, 10000, 100, 5, "BenchmarkInteger.xlsx");
+    WriteTest(-42, 1000, 1000, 5, "BenchmarkInteger.xlsx");
+    //WriteTest(-42, 100, 10000, 5, "BenchmarkInteger.xlsx");
+
+    //WriteTest(3.14159, 1000000, 1, 5, "BenchmarkFloat.xlsx");
+    //WriteTest(3.14159, 100000, 10, 5, "BenchmarkFloat.xlsx");
+    //WriteTest(3.14159, 10000, 100, 5, "BenchmarkFloat.xlsx");
+    WriteTest(3.14159, 1000, 1000, 5, "BenchmarkFloat.xlsx");
+    //WriteTest(3.14159, 100, 10000, 5, "BenchmarkFloat.xlsx");
+
+    //WriteTest(true, 1000000, 1, 5, "BenchmarkBool.xlsx");
+    //WriteTest(true, 100000, 10, 5, "BenchmarkBool.xlsx");
+    //WriteTest(true, 10000, 100, 5, "BenchmarkBool.xlsx");
+    WriteTest(true, 1000, 1000, 5, "BenchmarkBool.xlsx");
+    //WriteTest(true, 100, 10000, 5, "BenchmarkBool.xlsx");
+
     return 0;
 }
 
 template<typename T>
-unsigned long WriteTest(T value, unsigned long rows, unsigned long columns, int repetitions)
+unsigned long WriteTest(T value, unsigned long rows, unsigned long columns, int repetitions, const std::string &fileName)
 {
     TablePrinter tp(&std::cout);
     tp.AddColumn("Run #", 10);
@@ -31,7 +71,10 @@ unsigned long WriteTest(T value, unsigned long rows, unsigned long columns, int 
     tp.AddColumn("Writing (cells/s)", 20);
     tp.AddColumn("Saving (ms)", 20);
 
-    tp.PrintTitle("Writing " + to_string(rows) + " x " + to_string(columns) + " cells");
+    stringstream ss;
+    ss << "Writing \"" << value << "\" to " << to_string(rows) << " x " << to_string(columns) << " cells";
+
+    tp.PrintTitle(ss.str());
     tp.PrintHeader();
 
     unsigned long TotalOpeningTime = 0;
@@ -42,7 +85,7 @@ unsigned long WriteTest(T value, unsigned long rows, unsigned long columns, int 
         // Create new document
         auto startOpen = chrono::steady_clock::now();
         OpenXLSX::XLDocument doc;
-        doc.CreateDocument("Benchmark.xlsx");
+        doc.CreateDocument(fileName);
         auto endOpen = chrono::steady_clock::now();
 
         // Add data to document
