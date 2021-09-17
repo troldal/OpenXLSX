9e4c08cb839c8d864c2d5aac6bc1987a458b98d7
troldal <kenneth.balslev@gmail.com>
2021-09-17 23:06:52 +0200

    Cleanup of demo programs

--- Examples/Demo5.cpp
+++ Examples/Demo5.cpp
@@ -23,28 +23,43 @@ int main()
     // The resulting XLCellRange objects provides 'begin()' and 'end()' (returning iterators),
     // so that the object can be used in a range-based for-loop.
 
-    cout << "\nGenerating spreadsheet (1,048,576 rows x 8 columns) ..." << endl;
+    // First, create a new document and access the sheet named 'Sheet1'.
+    cout << "\nGenerating spreadsheet ..." << endl;
     XLDocument doc;
     doc.create("./Demo05.xlsx");
     auto wks = doc.workbook().worksheet("Sheet1");
-    auto rng = wks.range(XLCellReference("A1"), XLCellReference(1048576, 8));
 
+    // Create a random-number generator (to be used later)
     std::random_device                 rand_dev;
     std::mt19937                       generator(rand_dev());
     std::uniform_int_distribution<int> distr(0, 99);
 
+    // Create a cell range using the 'range()' method of the XLWorksheet class.
+    // The 'range()' method takes two XLCellReference objects; one for the cell
+    // in the upper left corner, and one for the cell in the lower right corner.
+    // OpenXLSX defines two constants: MAX_ROWS which is the maximum number of
+    // rows in a worksheet, and MAX_COLS which is the maximum number of columns.
+    auto rng = wks.range(XLCellReference("A1"), XLCellReference(OpenXLSX::MAX_ROWS, 8));
+
+    // An XLCellRange object provides begin and end iterators, and can be used in
+    // range-based for-loops. The next line iterates through all the cells in the
+    // range and assigns a random number to each cell:
     for (auto& cell : rng) cell.value() = distr(generator);
 
-    cout << "Saving spreadsheet (1,048,576 rows x 8 columns) ..." << endl;
+    // Saving a large spreadsheet can take a while...
+    cout << "Saving spreadsheet ..." << endl;
     doc.save();
     doc.close();
 
-    cout << "Re-opening spreadsheet (1,048,576 rows x 8 columns) ..." << endl;
+    // Reopen the spreadsheet..
+    cout << "Re-opening spreadsheet ..." << endl;
     doc.open("./Demo05.xlsx");
     wks = doc.workbook().worksheet("Sheet1");
-    rng = wks.range(XLCellReference("A1"), XLCellReference(1048576, 8));
 
-    cout << "Reading data from spreadsheet (1,048,576 rows x 8 columns) ..." << endl;
+    // Create the range, count the cells in the range, and sum the numbers assigned
+    // to the cells in the range.
+    cout << "Reading data from spreadsheet ..." << endl;
+    rng = wks.range(XLCellReference("A1"), XLCellReference(OpenXLSX::MAX_ROWS, 8));
     cout << "Cell count: " << std::distance(rng.begin(), rng.end()) << endl;
     cout << "Sum of cell values: "
          << accumulate(rng.begin(), rng.end(), 0, [](uint64_t a, XLCell& b) { return a + b.value().get<int64_t>(); });
--- Examples/Demo6.cpp
+++ Examples/Demo6.cpp
@@ -13,63 +13,86 @@ int main()
     cout << "DEMO PROGRAM #06: Row Handling\n";
     cout << "********************************************************************************\n";
 
-    cout << "\nGenerating spreadsheet (1,048,576 rows x 8 columns) ..." << endl;
+    // As an alternative to using cell ranges, you can use row ranges.
+    // This can be significantly faster (up to twice as fast for both reading and
+    // writing).
+
+    // First, create a new document and access the sheet named 'Sheet1'.
+    cout << "\nGenerating spreadsheet ..." << endl;
     XLDocument doc;
-    std::string path = "./電子表格.xlsx";
+    std::string path = "./Demo06.xlsx";
     doc.create(path);
     auto wks = doc.workbook().worksheet("Sheet1");
 
+    // Create a random-number generator (to be used later)
     std::random_device                 rand_dev;
     std::mt19937                       generator(rand_dev());
     std::uniform_int_distribution<int> distr(0, 99);
 
+    // The XLWorksheet class has a 'rows()' method, that returns a XLRowRange
+    // object. Similar to XLCellRange, XLRowRange provides begin and end iterators,
+    // enabeling iteration through the individual XLRow objects.
     std::vector<XLCellValue> writeValues;
-
-    for (auto& row : wks.rows(1'048'576)) {
-        //    for (auto& row : wks.rows(10)) {
+    for (auto& row : wks.rows(OpenXLSX::MAX_ROWS)) {
         writeValues.clear();
-        writeValues.emplace_back(distr(generator));
-        writeValues.emplace_back(distr(generator));
-        writeValues.emplace_back(distr(generator));
-        writeValues.emplace_back(distr(generator));
-        writeValues.emplace_back(distr(generator));
-        writeValues.emplace_back(distr(generator));
-        writeValues.emplace_back(distr(generator));
-        writeValues.emplace_back(distr(generator));
+        for (int i = 0; i < 8; ++i) writeValues.emplace_back(distr(generator));
 
+        // The XLRow class provides a 'values()' method. Similar to the 'value()'
+        // method of the XLCell class, the 'values()' method returns a reference
+        // to a proxy object that facilitates implicit conversions etc.
+        // Using the 'values()' method, containers can be assigned to each row.
+        // In this case, a std::vector is used, but any container supporting
+        // bi-directional iterators (such as std::deque or std::list) can be
+        // used.
+        // In this example a vector of XLCellValue objects is used. This is
+        // useful if you heterogeneous data (such as doubles, ints and strings)
+        // in the same dataset. However, if you have homogeneous data, e.g. all
+        // doubles, you don't have to use XLCellValue objects. You can, for
+        // examlpe, assigne a std::vector of doubles directly to the row.
         row.values() = writeValues;
-//        row.values().clear();
     }
 
-    cout << "Saving spreadsheet (1,048,576 rows x 8 columns) ..." << endl;
+    // Saving a large spreadsheet can take a while...
+    cout << "Saving spreadsheet ..." << endl;
     doc.save();
     doc.close();
 
-    cout << "Re-opening spreadsheet (1,048,576 rows x 8 columns) ..." << endl;
+    // Reopen the spreadsheet...
+    cout << "Re-opening spreadsheet ..." << endl;
     doc.open(path);
     wks = doc.workbook().worksheet("Sheet1");
+
+    // Prepare for reading...
     uint64_t sum = 0;
     uint64_t count = 0;
     std::vector<XLCellValue> readValues;
+    cout << "Reading data from spreadsheet ..." << endl;
 
-    cout << "Reading data from spreadsheet (1,048,576 rows x 8 columns) ..." << endl;
-
+    // The 'values()' member function can be used for both reading and writing
+    // of data. In this case the 'readValues' variable is a std::vector of
+    // XLCellValues, and the 'values()' method will perform implicit conversion
+    // to the correct type. Again, all containers supporting bi-directional
+    // iterators are supported. Also, if you have a vector of doubles, the values
+    // will be converted, if possible. Note, however, that if conversion is not
+    // possible, an exception will be thrown. For that reason, it is often best
+    // to use a container of XLCellValue objects, and then later determine the
+    // data type for each object.
     for (auto& row : wks.rows()) {
-        readValues = row.values();//<std::vector<XLCellValue>>();
+        readValues = row.values();
+
+        // Count the number of cell values
         count += std::count_if(readValues.begin(), readValues.end(), [](const XLCellValue& v) {
             return v.type() != XLValueType::Empty;
         });
-    }
-    cout << "Cell count: " << count << endl;
 
-    for (auto& row : wks.rows()) {
-        readValues = row.values();//<std::vector<XLCellValue>>();
+        // Sum the numbers in each cell.
         sum += std::accumulate(readValues.begin(),
                                readValues.end(),
                                0,
                                [](uint64_t a, XLCellValue& b) {return a + b.get<uint64_t>(); });
     }
 
+    cout << "Cell count: " << count << endl;
     cout << "Sum of cell values: " << sum << endl;
 
     doc.close();
