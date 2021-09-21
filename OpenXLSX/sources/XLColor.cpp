775a69ab33633badb9a9019a520c20e173788756
U-NZAG\I22696 <I22696@W1102300.nordzucker.lan>
2021-09-21 14:06:11 +0200

    XLCell cleanup

--- OpenXLSX/sources/XLColor.cpp
+++ OpenXLSX/sources/XLColor.cpp
@@ -102,12 +102,28 @@ void XLColor::set(uint8_t red, uint8_t green, uint8_t blue)
  */
 void XLColor::set(const std::string& hexCode)
 {
-    if (hexCode.size() > 8) throw XLInputError("Invalid color code");
+    std::string alpha;
+    std::string red;
+    std::string green;
+    std::string blue;
 
-    std::string alpha = hexCode.substr(0, 2);
-    std::string red   = hexCode.substr(2, 2);
-    std::string green = hexCode.substr(4, 2);
-    std::string blue  = hexCode.substr(6, 2);
+    auto temp = hex();
+
+    if (hexCode.size() == 6) {
+        alpha = hex().substr(0, 2);
+        red   = hexCode.substr(0, 2);
+        green = hexCode.substr(2, 2);
+        blue  = hexCode.substr(4, 2);
+    }
+
+    else if (hexCode.size() == 8) {
+        alpha = hexCode.substr(0, 2);
+        red   = hexCode.substr(2, 2);
+        green = hexCode.substr(4, 2);
+        blue  = hexCode.substr(6, 2);
+    }
+    else
+        throw XLInputError("Invalid color code");
 
     m_alpha = static_cast<uint8_t>(stoul(alpha, nullptr, 16));
     m_red   = static_cast<uint8_t>(stoul(red, nullptr, 16));
