030842fb0cf4e80336cf1f22bfaf2b644c5b0e4f
isajkin <55384041+isajkin@users.noreply.github.com>
2025-04-22 09:21:14 +0300

    Add files via upload

--- OpenXLSX/headers/XLDrawing1.hpp
+++ OpenXLSX/headers/XLDrawing1.hpp
@@ -14,7 +14,14 @@
 
 using namespace OpenXLSX;
 
+void setAttribute(XMLNode n, char* path, char* attribute, char* value);
+
+class XLPictureFormat1;
+class XLTextFrame1;
 class XLShape1;
+class XLCharacters2;
+class XLFill2;
+class XLFont2;
 
 class XLDrawing1 : public XLXmlFile
 {
@@ -39,6 +46,7 @@ public:
 	uint32_t shapeCount() const;
 
 	XMLNode rootNode() const;
+    void setShapeAttribute(uint32_t shapeIndex,char* path, char* attribute, char* value);
 
 private:
 	XMLNode firstShapeNode() const;
@@ -50,49 +58,125 @@ private:
 	std::string m_defaultShapeTypeId{};
 };
 
+class XLFill2 : public XMLNode
+{
+public:
+    XLFill2();
+    explicit XLFill2(const XMLNode& node);
+    ~XLFill2() = default;
+    XMLNode* xmlnode() { return m_node.get(); };
+    void reset() { m_node.reset(); };
+private:
+    std::unique_ptr<XMLNode> m_node;
+};
+
+class XLPictureFormat1 : public XMLNode
+{
+public:
+    XLPictureFormat1();
+    explicit XLPictureFormat1(const XMLNode& node);
+    ~XLPictureFormat1() = default;
+    XMLNode* xmlnode() { return m_node.get(); };
+    void reset() { m_node.reset(); };
+    XMLNode setBlipFill();
+    XMLNode setPrstGeom(char *prst);
+    XMLNode setSolidFill();
+    XMLNode setXfrm();
+    XMLNode setLn();
+private:
+    std::unique_ptr<XMLNode> m_node;
+};
+
+class XLTextFrame1 : public XMLNode
+{
+public:
+    XLTextFrame1();
+    explicit XLTextFrame1(const XMLNode& node);
+    ~XLTextFrame1() = default;
+    XLShape1* s1() { return m_s1; };
+    XLCharacters2 Characters();
+    XMLNode *xmlnode() { return m_node.get(); };
+    void reset() { m_node.reset(); };
+    void setVerticalAlignment(std::string val);
+    void setHorizontalAlignment(std::string val);
+private:
+    XLShape1* m_s1;
+    std::unique_ptr<XMLNode> m_node;
+};
+
+class XLCharacters2
+{
+public :
+    XLCharacters2();
+    explicit XLCharacters2(const XMLNode& node);
+    ~XLCharacters2() = default;
+    XMLNode* xmlnode() { return m_node.get(); };
+    void reset(){m_node.reset();};
+    void setText(std::string text);
+    XLFont2 font();
+private :
+    std::unique_ptr<XMLNode> m_node;
+};
+
+class XLFont2
+{
+public:
+    XLFont2();
+    explicit XLFont2(const XMLNode& node);
+    ~XLFont2() = default;
+    XMLNode* xmlnode() { return m_node.get(); };
+    void reset() { m_node.reset(); };
+    void setBold(bool val);
+    void setItalic(bool val);
+    void setStrikethrough(bool val);
+    void setUnderline(int32_t val);
+    void setSize(int32_t val);
+private:
+    std::unique_ptr<XMLNode> m_node;
+};
+
 class XLShape1 {
         friend class XLDrawing1;
     public:
         XLShape1();
         explicit XLShape1(const XMLNode& node);
-	XLShape1(const XLShape1& other) = default;
+	    XLShape1(const XLShape1& other) = default;
         XLShape1(XLShape1&& other) noexcept = default;
        ~XLShape1() = default;
         XLShape1& operator=(const XLShape1& other);
         XLShape1& operator=(XLShape1&& other) noexcept = default;
-
-        std::string shapeId() const;   // v:shape attribute id - shape_# - can't be set by the user
-        std::string fillColor() const; // v:shape attribute fillcolor, #<3 byte hex code>, e.g. #ffffc0
-        bool stroked() const;          // v:shape attribute stroked "t" ("f"?)
-        std::string type() const;      // v:shape attribute type, link to v:shapetype attribute id
-        bool allowInCell() const;      // v:shape attribute o:allowincell "f"
-//        XLShapeStyle style();          // v:shape attribute style, but constructed from the XMLAttribute
-
-        // XLShapeShadow& shadow();          // v:shape subnode v:shadow
-        // XLShapeFill& fill();              // v:shape subnode v:fill
-        // XLShapeStroke& stroke();          // v:shape subnode v:stroke
-        // XLShapePath& path();              // v:shape subnode v:path
-        // XLShapeTextbox& textbox();        // v:shape subnode v:textbox
-//        XLShapeClientData clientData();  // v:shape subnode x:ClientData
-
-        // NOTE: setShapeId is not available because shape id is managed by the parent class in createShape
- 
-       bool setFillColor(std::string const& newFillColor);
-        bool setStroked(bool set);
-        bool setType(std::string const& newType);
-        bool setAllowInCell(bool set);
-        bool setStyle(std::string const& newStyle);
-  //      bool setStyle(XLShapeStyle const& newStyle);
-
+        XMLNode *xmlnode() { return m_shapeNode.get();};
+        void reset() { m_shapeNode.reset(); };
+        XLTextFrame1 textFrame();
+        XLPictureFormat1 pictureFormat();
+        XLFill2 fill();
+        void from(int row, int col, int rowoff, int coloff);
+        void to(int row, int col, int rowoff, int coloff);
+        void ext(int cx, int cy);
+        XMLNode cxnSp();
+        XMLNode sp();
+        XMLNode pic();
+        XMLNode setCxnSp(char *macro);
+        XMLNode setSp(char *macro,char *textlink);
+        XMLNode setPic(char *macro);
+        void setName(std::string name);
+        void setId(int32_t id);
+        void setRotation(float rot);
+        void setLeft(float left);
+        void setTop(float top);
+        void setWidth(float width);
+        void setHeight(float height);
+        std::string name();
+        float rotation();
+        float left();
+        float top();
+        float width();
+        float height();
+        XMLNode setClientData();
     private:
-        std::unique_ptr<XMLNode> m_shapeNode;        /**< An XMLNode object with the v:shape item */
+        std::unique_ptr<XMLNode> m_shapeNode;
         inline static const std::vector< std::string_view > m_nodeOrder = {
-            "v:shadow",
-            "v:fill",
-            "v:stroke",
-            "v:path",
-            "v:textbox",
-            "x:ClientData"
+            "xdr:ClientData"
         };
     }; 
 
